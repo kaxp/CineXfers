63ecf97ca04148f7d53662ed6c076b66bfb640ba
kaxp <kapilsahu232@gmail.com>
2024-01-17 23:04:18 +0530

    Refactor depricated apis

--- lib/screens/detail/detail_page.dart
+++ lib/screens/detail/detail_page.dart
@@ -17,77 +17,73 @@ import '../../utils/app_divider.dart';
 class DetailPage extends GetView<DetailController> {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<DetailController>(builder: (controller) {
-      return Scaffold(
-        appBar: CineAppBar(
-          isCenter: false,
-          titleWidget: Header5(
-            title: controller.movieData.title.toString(),
-            color: Colors.white,
-            maxLines: 1,
-            textOverflow: TextOverflow.ellipsis,
-          ),
-        ),
-        body: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                DetailPageHeader(
-                  imageUrl: controller.movieData.bannerPath.toString(),
-                  data: controller.movieData,
-                  onCalendarTap: () => controller.showSnackbar(),
-                  onLocationTap: () => controller.showSnackbar(),
-                  onTrailerTap: () => controller.showSnackbar(),
-                ),
-                AppDivider.divider,
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
-                  child: Header6(
-                    title: S.current.about,
-                    color: AppColors.redColor,
-                    fontFamily: AppFonts.ratBold,
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.only(
-                      left: 16.0, right: 16.0, bottom: 20, top: 4),
-                  child: Header2(
-                      title: controller.movieData.overview.toString(),
-                      maxLines: null,
-                      color: Colors.black87,
-                      fontWeight: FontWeight.normal,
-                      fontFamily: AppFonts.ratMedium),
-                ),
-                AppDivider.divider,
-                TimeSelectionList(
-                  data: controller.movieData,
-                  onButtonTapped: (childIndex, parentIndex) {
-                    controller.updateTimeSelectionCellIndex(
-                        childIndex, parentIndex);
-                  },
-                  childListIndex: controller.childListIndex,
-                  parentListIndex: controller.parentListIndex,
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16, 40, 16, 16),
-                  child: DefaultElevatedButton(
-                    title: S.current.bookNow,
-                    onPressed: controller.parentListIndex != -1
-                        ? () => controller.showSnackbar()
-                        : null,
-                    primaryColor: controller.parentListIndex != -1
-                        ? AppColors.redColor
-                        : AppColors.disabledColor,
-                  ),
-                ),
-              ],
+    return GetBuilder<DetailController>(
+      builder: (controller) {
+        return Scaffold(
+          appBar: CineAppBar(
+            isCenter: false,
+            titleWidget: Header5(
+              title: controller.movieData.title.toString(),
+              color: Colors.white,
+              maxLines: 1,
+              textOverflow: TextOverflow.ellipsis,
             ),
-          ],
-        ),
-      );
-    });
+          ),
+          body: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  DetailPageHeader(
+                    imageUrl: controller.movieData.bannerPath.toString(),
+                    data: controller.movieData,
+                    onCalendarTap: () => controller.showSnackbar(),
+                    onLocationTap: () => controller.showSnackbar(),
+                    onTrailerTap: () => controller.showSnackbar(),
+                  ),
+                  AppDivider.divider,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+                    child: Header6(
+                      title: S.current.about,
+                      color: AppColors.redColor,
+                      fontFamily: AppFonts.ratBold,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 20, top: 4),
+                    child: Header2(
+                        title: controller.movieData.overview.toString(),
+                        maxLines: null,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: AppFonts.ratMedium),
+                  ),
+                  AppDivider.divider,
+                  TimeSelectionList(
+                    data: controller.movieData,
+                    onButtonTapped: (childIndex, parentIndex) {
+                      controller.updateTimeSelectionCellIndex(childIndex, parentIndex);
+                    },
+                    childListIndex: controller.childListIndex,
+                    parentListIndex: controller.parentListIndex,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16, 40, 16, 16),
+                    child: DefaultElevatedButton(
+                      title: S.current.bookNow,
+                      onPressed: controller.parentListIndex != -1 ? () => controller.showSnackbar() : null,
+                      primaryColor: controller.parentListIndex != -1 ? AppColors.redColor : AppColors.disabledColor,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
